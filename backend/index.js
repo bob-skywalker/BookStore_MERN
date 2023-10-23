91453c6142cbe66c77fdba92530d9191250d3131
bobo <q463250938@gmail.com>
2023-10-23 15:15:39 -0700

    update book express function added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,18 @@ import { Book } from "./models/bookModel.js";
 
 const { Schema } = mongoose;  
 
+mongoose 
+    .connect(mongoDBURL)
+    .then(()=> {
+        console.log('APP connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
 const app = express();
 app.use(express.json());
 
@@ -16,7 +28,7 @@ app.get('/', (req, res) => {
     return res.status(234).send("Welcome to MERN!")
 })
 
-//Route for save a new book 
+//Route for save a new book  
 app.post('/books', async (req, res) => {
     try {
         if (
@@ -44,16 +56,60 @@ app.post('/books', async (req, res) => {
     }
 });
 
-
-mongoose 
-    .connect(mongoDBURL)
-    .then(()=> {
-        console.log('APP connected to MongoDB');
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
+// Routes to get All Books from Mongoose 
+app.get('/books', async (req, res) => {
+    try{
+        const books = await Book.find({});
+        return res.status(200).json({
+            count: books.length,
+            data: books
         });
-    })
-    .catch((err) => {
+
+    } catch (err){
+        console.log(err.message);
+        res.status(500).send({message: err.message});
+    }
+});
+
+// Routes to get one book from Mongoose 
+app.get('/books/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+
+        const book = await Book.findById(id);
+        return res.status(200).json(book);
+
+    } catch(err) {
         console.log(err)
-    })
+        res.status(500).send({message: err.message});
+    }
+});
+
+// Routes for Update a Book 
+app.put('/books/:id', async(req, res) => {
+    try{
+        if (!req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+            ) {
+                return res.status(400).send({
+                    message: 'Send all required fields: title, author, publishYear'
+                });
+            }
+        const { id } = req.params;
+
+        const book = await Book.findByIdAndUpdate(id, req.body);
+
+        if (!book) {
+            res.status(404).send({message: 'Book not found'});
+        }
+
+        return res.status(200).json({message: 'Book updated sucessfully.'});
+
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+});
 
