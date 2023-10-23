1b38932b0d06b0339ac5108477b6108a05b8b2b4
bobo <q463250938@gmail.com>
2023-10-23 13:13:58 -0700

    post method and book schema implemented && testing for creating new book worked

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,47 @@ import express from "express";
 import { PORT } from "./config.js";
 import { mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const { Schema } = mongoose;  
 
 const app = express();
+app.use(express.json());
 
+
+//setting default URL path 
 app.get('/', (req, res) => {
     console.log(req)
     return res.status(234).send("Welcome to MERN!")
 })
+
+//Route for save a new book 
+app.post('/books', async (req, res) => {
+    try {
+        if (
+            !req.body.title || 
+            !req.body.author || 
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields: title, author, publish year'
+            })
+        }
+        const newBook = new Book({
+            title: req.body.title, 
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        });
+
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book);
+
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send({message: err.message});
+    }
+});
 
 
 mongoose 
---
+++ backend/models/bookModel.js
@@ -0,0 +1,21 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose"; 
+
+const bookSchema = new Schema({
+    title: {
+        type: String, 
+        required: true 
+    },
+    author: {
+        type: String,
+        required: true 
+    },
+    publishYear: {
+        type: Number, 
+        required: true 
+    }
+}, { 
+    timestamps: true 
+});
+
+export const Book = mongoose.model('Book', bookSchema);
