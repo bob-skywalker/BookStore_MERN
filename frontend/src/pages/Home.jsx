7d366dc5d911c052c4a169b1f3df3813117c73e9
bobo <q463250938@gmail.com>
2023-10-24 13:50:17 -0700

    create book functionality finished

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,13 +23,13 @@ const Home = () => {
             console.log(err);
             setLoading(false);
         })
-  }, []);
+  },[]);
 
   return (
     <div className='p-4'>
        <div className='flex justify-between items-center'>
             <h1 className='text-3xl my-8'>Book List</h1>
-                <Link to='/books/create'>
+                <Link to='/book/create'>
                     <MdOutlineAddBox className='text-sky-800 text-4xl' />
                 </Link>
        </div>
